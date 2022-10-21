4a25d25f5fcb3c318a481d1065f3ff0e0956269d
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2022-10-21 09:44:20 +0300

    1749C - WA

--- 1749/1749C.py
+++ 1749/1749C.py
@@ -1,19 +1,20 @@
-from collections import Counter
+from bisect import bisect_right
 
 
 def solution():
     n = int(input())
     a = [int(num) for num in input().split()]
-    c = Counter(a)
+    a.sort()
     k = 1
-    while k < 101:
-        if c.get(1, 0) < k:
+    while k < n + 1:
+        i = bisect_right(a, 1)
+        if i < k:
             print(k - 1)
             return
-        for i in range(2, k + 1):
-            if c.get(i, 0) < 1:
-                print(k - 1)
-                return
+        i = bisect_right(a, k)
+        if i < 2 * k - 1:
+            print(k - 1)
+            return
         k += 1
     print(k - 1)
 
