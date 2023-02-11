af7a47169a5d9e70be2a0f487ce595e3eb953622
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2023-02-11 13:26:17 +0300

    template

--- templates/A.py
+++ templates/A.py
@@ -1,8 +1,7 @@
 import sys
 
 input = sys.stdin.readline
-print_list = lambda arr: sys.stdout.write(" ".join(map(str, arr)) + "\n")
-
+print_row = lambda row: sys.stdout.write(" ".join(map(str, row)) + "\n")
 
 
 def solution():
@@ -10,12 +9,12 @@ def solution():
     a = [int(num) for num in input().split()]
 
 
-
 def main():
     t = int(input())
     while t:
         solution()
         t -= 1
+        print()
 
 
 if __name__ == '__main__':
