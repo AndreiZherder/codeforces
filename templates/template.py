bb75773abfe87152a326ed7138ff9fe716c81172
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2023-02-27 14:16:21 +0300

    template

--- templates/template.py
+++ templates/template.py
@@ -244,7 +244,7 @@ def prefix_sum_2d(grid: List[List[int]]) -> List[List[int]]:
     """
     n = len(grid)
     m = len(grid[0])
-    pref = [[0 for i in range(m + 1)] for j in range(n + 1)]
+    pref = [[0 for j in range(m + 1)] for i in range(n + 1)]
 
     for i in range(1, n + 1):
         for j in range(1, m + 1):
