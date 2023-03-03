0162d861d8bb4b229b965b15cfb0050e0053bf56
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2023-03-03 10:28:10 +0300

    1800 - A1

---
+++ 1800/A1.py
@@ -0,0 +1,29 @@
+import sys
+from itertools import groupby
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def print(*args, sep=' ', end='\n'):
+    sys.stdout.write(sep.join(map(str, args)))
+    sys.stdout.write(end)
+
+
+def solution():
+    n = int(input())
+    s = input().lower()
+    if ''.join(key for key, group in groupby(s)) == 'meow':
+        print('YES')
+    else:
+        print('NO')
+
+
+def main():
+    t = int(input())
+    while t:
+        solution()
+        t -= 1
+
+
+if __name__ == '__main__':
+    main()
