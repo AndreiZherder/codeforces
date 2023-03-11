b8bab8b9f93ab9247e5b3d7ed198dcc705a5eb03
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2023-03-11 20:19:00 +0300

    1760 - G

--- 1760/1760G.py
+++ 1760/1760G.py
@@ -1,24 +1,45 @@
-from collections import defaultdict
+import sys
+from collections import deque, defaultdict
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def print(*args, sep=' ', end='\n'):
+    sys.stdout.write(sep.join(map(str, args)))
+    sys.stdout.write(end)
 
 
 def solution():
-    n, a, b = (int(num) for num in input().split())
-    edges = []
+    n, a, b = [int(num) for num in input().split()]
+    g = defaultdict(list)
     for i in range(n - 1):
-        edges.append((int(num) for num in input().split()))
-    tree = defaultdict(list)
-    for v, u, w in edges:
-        tree[v].append((u, w))
-        tree[u].append((v, w))
-    print(tree)
+        u, v, w = [int(num) for num in input().split()]
+        g[u].append((v, w))
+        g[v].append((u, w))
 
+    vals = set()
+    q = deque([(b, 0)])
+    seen = {b}
+    while q:
+        u, x = q.popleft()
+        for v, w in g[u]:
+            if v not in seen:
+                vals.add(x ^ w)
+                q.append((v, x ^ w))
+                seen.add(v)
 
-
-
-
-
-
-
+    q = deque([(a, 0)])
+    seen = {a, b}
+    while q:
+        u, x = q.popleft()
+        if x in vals:
+            print('YES')
+            return
+        for v, w in g[u]:
+            if v not in seen:
+                q.append((v, x ^ w))
+                seen.add(v)
+    print('NO')
 
 
 def main():
