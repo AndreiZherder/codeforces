15643010ea40372794908ae6a4fb3f21639a850e
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2023-06-08 16:08:10 +0300

    template

---
+++ templates/mod.py
@@ -0,0 +1,91 @@
+from typing import Tuple
+
+
+def egcd(a: int, b: int) -> Tuple[int, int, int]:
+    """
+    Given two integers a and b, returns (gcd(a, b), x, y) such that
+    a * x + b * y == gcd(a, b).
+    """
+    x0, x1, y0, y1 = 1, 0, 0, 1
+    while b != 0:
+        q, a, b = a // b, b, a % b
+        x0, x1 = x1, x0 - q * x1
+        y0, y1 = y1, y0 - q * y1
+    return a, x0, y0
+
+
+def diophantine(a: int, b: int, c: int) -> Tuple[int, int]:
+    """
+    solves a * x + b * y = c
+    has solution only if c % gcd(a, b) == 0
+    returns (x0, y0)
+    other solutions for equation are:
+    x = x0 + k * b // gcd(a, b)
+    y = y0 - k * a // gcd(a, b)
+    """
+    g, x, y = egcd(a, b)
+    r = c // g
+    return r * x, r * y
+
+
+mod = 1000000007
+
+
+def madd(x: int, y: int) -> int:
+    return (x + y) % mod
+
+
+def msub(x: int, y: int) -> int:
+    return (x - y) % mod
+
+
+def mmul(x: int, y: int) -> int:
+    return (x * y) % mod
+
+
+def minv(x: int) -> int:
+    """
+    returns modular inverse of x when mod is prime or not prime
+    ans = (1 / x) % mod
+    gcd(y, mod) should be 1
+    works 2 times faster than pow(x, mod - 2, mod)
+    """
+    g, inv, _ = egcd(x, mod)
+    if g != 1:
+        raise ValueError('gcd(x, mod) != 1')
+    return inv % mod
+
+
+# def minv(x: int) -> int:
+#     """
+#     returns modular inverse of x when mod is prime
+#     ans = (1 / x) % mod
+#     uses Fermat's little theorem
+#     """
+#     return pow(x, mod - 2, mod)
+
+
+def mdiv(x: int, y: int) -> int:
+    """
+    returns (x / y) % mod
+    gcd(y, mod) should be 1
+    """
+    return (x * minv(y)) % mod
+
+
+def mpow(x: int, y: int) -> int:
+    """
+    returns (x ** y) % mod
+    """
+    return pow(x, y, mod)
+
+
+def ncr(n: int, r: int) -> int:
+    """
+    returns number of ways for selecting r elements out of n options
+    """
+    num, den = 1, 1
+    for i in range(r):
+        num = mmul(num, n - i)
+        den = mmul(den, i + 1)
+    return mdiv(num, den)
