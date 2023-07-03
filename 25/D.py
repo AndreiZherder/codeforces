f7dd49e3a3c7813e2a9367c968994e8d840f96e0
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2023-07-03 15:58:42 +0300

    25 - D

---
+++ 25/D.py
@@ -0,0 +1,64 @@
+from sys import stdin, stdout
+
+
+def input():
+    return stdin.readline().rstrip()
+
+
+def print(*args, sep=' ', end='\n'):
+    stdout.write(sep.join(map(str, args)))
+    stdout.write(end)
+
+
+class DSU:
+    def __init__(self, n: int):
+        self.n = n
+        self.parent = list(range(n))
+        self.rank = [1] * n
+
+    def find(self, i: int) -> int:
+        if self.parent[i] != i:
+            self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
+
+    def union(self, i: int, j: int):
+        id1 = self.find(i)
+        id2 = self.find(j)
+        if id1 == id2:
+            return
+        if self.rank[id1] > self.rank[id2]:
+            self.parent[id2] = id1
+        else:
+            self.parent[id1] = id2
+            if self.rank[id1] == self.rank[id2]:
+                self.rank[id2] += 1
+
+
+def solution():
+    n = int(input())
+    dsu = DSU(n)
+    back_edges = []
+    for i in range(n - 1):
+        v, u = (int(num) for num in input().split())
+        v -= 1
+        u -= 1
+        if dsu.find(v) == dsu.find(u):
+            back_edges.append((v + 1, u + 1))
+        else:
+            dsu.union(v, u)
+    leaders = list({dsu.find(i) for i in range(n)})
+    ans = len(leaders) - 1
+    print(ans)
+    for i in range(ans):
+        print(*back_edges.pop(), leaders[0] + 1, leaders[i + 1] + 1)
+
+
+def main():
+    t = 1
+    while t:
+        solution()
+        t -= 1
+
+
+if __name__ == '__main__':
+    main()
