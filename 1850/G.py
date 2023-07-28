35b142ec27221a10b77eff129be7ef31a8d26e66
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2023-07-28 14:29:55 +0300

    1850 - G

--- 1850/G.py
+++ 1850/G.py
@@ -1,5 +1,6 @@
 from collections import Counter
 from math import comb
+from random import getrandbits
 from sys import stdin, stdout
 
 
@@ -12,6 +13,14 @@ def print(*args, sep=' ', end='\n'):
     stdout.write(end)
 
 
+RANDOM = getrandbits(32)
+
+
+class Int(int):
+    def __hash__(self):
+        return super().__hash__() ^ RANDOM
+
+
 def solution():
     n = int(input())
     xs = Counter()
@@ -19,11 +28,11 @@ def solution():
     deltas1 = Counter()
     deltas2 = Counter()
     for i in range(n):
-        x, y = (int(num) for num in input().split())
-        xs[str(x)] += 1
-        ys[str(y)] += 1
-        deltas1[str(x + y)] += 1
-        deltas2[str(y - x)] += 1
+        x, y = (Int(num) for num in input().split())
+        xs[x] += 1
+        ys[y] += 1
+        deltas1[x + y] += 1
+        deltas2[y - x] += 1
     ans = 0
     for v in xs.values():
         ans += comb(v, 2)
