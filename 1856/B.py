2132bfc3da9299e814f8ee2c2054450cebd16849
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2023-08-05 22:04:16 +0300

    1856 - AB

---
+++ 1856/B.py
@@ -0,0 +1,38 @@
+from os import path
+from sys import stdin, stdout
+
+
+if path.exists("../templates/input.txt"):
+    stdin = open("../templates/input.txt", 'r')
+
+
+def input():
+    return stdin.readline().rstrip()
+
+
+def print(*args, sep=' ', end='\n'):
+    stdout.write(sep.join(map(str, args)))
+    stdout.write(end)
+
+
+def solution():
+    n = int(input())
+    nums = [int(num) for num in input().split()]
+    if n == 1:
+        print('NO')
+        return
+    if sum(nums) - nums.count(1) >= n:
+        print('YES')
+    else:
+        print('NO')
+
+
+def main():
+    t = int(input())
+    while t:
+        solution()
+        t -= 1
+
+
+if __name__ == '__main__':
+    main()
