2e884ba0e5c55ed4e08a12fb3ce2c3c602357df8
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2023-08-22 18:35:40 +0300

    1102 - C

---
+++ 1102/C.py
@@ -0,0 +1,37 @@
+from os import path
+from sys import stdin, stdout
+
+
+filename = "../templates/input.txt"
+if path.exists(filename):
+    stdin = open(filename, 'r')
+
+
+def input():
+    return stdin.readline().rstrip()
+
+
+def print(*args, sep=' ', end='\n'):
+    stdout.write(sep.join(map(str, args)))
+    stdout.write(end)
+
+
+def solution():
+    n, x, y = [int(num) for num in input().split()]
+    nums = [int(num) for num in input().split()]
+    if x > y:
+        print(n)
+    else:
+        cnt = sum(num <= x for num in nums)
+        print((cnt + 1) // 2)
+
+
+def main():
+    t = 1
+    while t:
+        solution()
+        t -= 1
+
+
+if __name__ == '__main__':
+    main()
