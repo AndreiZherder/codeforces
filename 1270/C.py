022fc877a8148e00b2905859572f12b5e0bf3d18
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2023-09-20 22:56:30 +0300

    1270 - C

---
+++ 1270/C.py
@@ -0,0 +1,38 @@
+from functools import reduce
+from operator import xor
+from os import path
+from sys import stdin, stdout
+
+
+filename = "../templates/input.txt"
+if path.exists(filename):
+    stdin = open(filename, 'r')
+
+
+def input():
+    return stdin.readline().rstrip()
+
+
+def print(*args, sep=' ', end='\n'):
+    stdout.write(sep.join(map(str, args)))
+    stdout.write(end)
+
+
+def solution():
+    n = int(input())
+    nums = [int(num) for num in input().split()]
+    y = reduce(xor, nums)
+    total = sum(nums)
+    print(2)
+    print(y, total + y)
+
+
+def main():
+    t = int(input())
+    while t:
+        solution()
+        t -= 1
+
+
+if __name__ == '__main__':
+    main()
