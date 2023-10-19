7577796fd2ca59f493107b915afa88058ea87a0d
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2023-10-19 10:20:55 +0300

    88 - C

---
+++ 88/C.py
@@ -0,0 +1,42 @@
+from math import gcd
+from os import path
+from sys import stdin, stdout
+
+
+filename = "../templates/input.txt"
+if path.exists(filename):
+    stdin = open(filename, 'r')
+
+
+def input():
+    return stdin.readline().rstrip()
+
+
+def print(*args, sep=' ', end='\n'):
+    stdout.write(sep.join(map(str, args)))
+    stdout.write(end)
+
+
+def solution():
+    a, b = [int(num) for num in input().split()]
+    g = gcd(a, b)
+    a //= g
+    b //= g
+    if abs(a - b) == 1:
+        ans = 'Equal'
+    elif a < b:
+        ans = 'Dasha'
+    else:
+        ans = 'Masha'
+    print(ans)
+
+
+def main():
+    t = 1
+    while t:
+        solution()
+        t -= 1
+
+
+if __name__ == '__main__':
+    main()
