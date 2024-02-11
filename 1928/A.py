1360e2c6f164b02397fd160f95e630661f4a2688
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2024-02-11 20:56:46 +0300

    1928 - AB

---
+++ 1928/A.py
@@ -0,0 +1,35 @@
+from os import path
+from sys import stdin, stdout
+
+
+filename = "../templates/input.txt"
+if path.exists(filename):
+    stdin = open(filename, 'r')
+
+
+def input():
+    return stdin.readline().rstrip()
+
+
+def print(*args, sep=' ', end='\n'):
+    stdout.write(sep.join(map(str, args)))
+    stdout.write(end)
+
+
+def solution():
+    a, b = [int(num) for num in input().split()]
+    if (a % 2 == 0 and a // 2 != b) or (b % 2 == 0 and b // 2 != a):
+        print('YES')
+    else:
+        print('NO')
+
+
+def main():
+    t = int(input())
+    while t:
+        solution()
+        t -= 1
+
+
+if __name__ == '__main__':
+    main()
