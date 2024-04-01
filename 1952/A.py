82054cb15f97e80ff19bdd27f655fb4226fb2a78
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2024-04-01 19:36:57 +0300

    1952 - AB

---
+++ 1952/A.py
@@ -0,0 +1,29 @@
+from os import path
+from sys import stdin, stdout
+
+
+filename = '../templates/input.txt'
+if path.exists(filename):
+    stdin = open(filename, 'r')
+
+
+def input():
+    return stdin.readline().rstrip()
+
+
+def print(*args, sep=' ', end='\n'):
+    stdout.write(sep.join(map(str, args)))
+    stdout.write(end)
+
+
+def solution():
+    print('safety')
+
+
+
+def main():
+    solution()
+
+
+if __name__ == '__main__':
+    main()
