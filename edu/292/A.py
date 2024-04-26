8642a0bc58b00bafcae1a4ddc4445beaf9aee18d
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2024-04-26 23:13:18 +0300

    edu292 - A

---
+++ edu/292/A.py
@@ -0,0 +1,42 @@
+from os import path
+from sys import stdin, stdout
+
+
+filename = '../../templates/input.txt'
+if path.exists(filename):
+    stdin = open(filename, 'r')
+
+
+def input():
+    return stdin.readline().rstrip()
+
+
+def print(*args, sep=' ', end='\n'):
+    stdout.write(sep.join(map(str, args)))
+    stdout.write(end)
+
+
+def solution():
+    n, s = [int(num) for num in input().split()]
+    nums = [int(num) for num in input().split()]
+    j = 0
+    best = 0
+    total = 0
+    for i in range(n):
+        total += nums[i]
+        while total > s:
+            total -= nums[j]
+            j += 1
+        best = max(best, i - j + 1)
+    print(best)
+
+
+def main():
+    t = 1
+    while t:
+        solution()
+        t -= 1
+
+
+if __name__ == '__main__':
+    main()
