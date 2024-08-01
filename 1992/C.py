a853a18b3352ec3be2e682f35db38acfcce568ed
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2024-08-01 22:14:24 +0300

    1992 - C

---
+++ 1992/C.py
@@ -0,0 +1,34 @@
+from os import path
+from sys import stdin, stdout
+
+
+filename = '../templates/input.txt'
+if path.exists(filename):
+    stdin = open(filename, 'r')
+
+
+def input():
+    return stdin.readline().rstrip()
+
+
+def print(*args, sep=' ', end='\n'):
+    stdout.write(sep.join(map(str, args)))
+    stdout.write(end)
+
+
+def solution():
+    n, m, k = [int(num) for num in input().split()]
+    ans = list(range(n, k - 1, -1)) + list(range(m + 1, k)) + list(range(1, m + 1))
+    print(*ans)
+
+
+
+def main():
+    t = int(input())
+    while t:
+        solution()
+        t -= 1
+
+
+if __name__ == '__main__':
+    main()
